8fb617c60aba29af954a0ed24c7840a43cf05acd
Krishna Glick <unstoppable411@gmail.com>
2017-04-15 16:17:54 -0400

    test case 1

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,12 @@ FROM node:7.9.0
 WORKDIR /cosmere-server
 ADD ./ /cosmere-server
 
+ENV MONGO_URL '0.0.0.0:27017'
+
 # Install Dependencies
 RUN npm i -g pm2
 RUN npm i
 RUN npm run build
+RUN npm run config
 
 CMD ["npm", "run", "prod"]
--- config-generator.js
+++ config-generator.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 
+const [,,force] = process.argv;
 
 //Edit me to add configs! I should be run as part of CI.
 const configTemplate = {
@@ -19,7 +20,9 @@ try {
 }
 catch(x) {}
 
-if(currentConfig)
+if(currentConfig) {
   _.forEach(Object.keys(configTemplate), key => currentConfig[key] = currentConfig[key] || configTemplate[key]);
-
-fs.writeFileSync(path.resolve('./config.json'), JSON.stringify(currentConfig, null, 2), { encoding: 'UTF8' });
+  fs.writeFileSync(path.resolve('./config.json'), JSON.stringify(currentConfig, null, 2), { encoding: 'UTF8' });
+}
+if(force)
+  fs.writeFileSync(path.resolve('./config.json'), JSON.stringify(configTemplate, null, 2), { encoding: 'UTF8' });
