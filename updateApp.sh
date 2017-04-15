83388ddc56767ad9ce9bc7fed6bd71b1e4fe6815
Krishna Glick <unstoppable411@gmail.com>
2017-04-15 16:33:44 -0400

    fuckit

--- updateApp.sh
+++ updateApp.sh
@@ -3,6 +3,7 @@ ssh 107.170.78.145 -l root << 'ENDSSH'
 cd ~/cosmere-server
 git pull origin-ssh master
 yarn
-docker ps -a | awk '{ print $1,$2 }' | grep cosmere | awk '{print $1 }' | xargs -I {} docker rm {} -f
-docker-compose up -d --build
+npm run prod &
+#docker ps -a | awk '{ print $1,$2 }' | grep cosmere | awk '{print $1 }' | xargs -I {} docker rm {} -f
+#docker-compose up -d --build
 ENDSSH
